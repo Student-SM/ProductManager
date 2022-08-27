7fb7022aa16751aef79042a43c86ea33323b82af
murin <murin.stanislav@mail.ru>
2022-08-27 16:17:21 +0300

    Сделал пункт 1 и 2

---
+++ src/main/java/ru/netology/ProductManager/Smartphone.java
@@ -0,0 +1,21 @@
+package ru.netology.ProductManager;
+
+public class Smartphone extends Product {
+
+    private String manufacturer;
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public Smartphone(int id, String name, int price, String manufacturer) {
+        super(id, name, price);
+        this.manufacturer = manufacturer;
+    }
+
+}
+
